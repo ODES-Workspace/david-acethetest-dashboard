cf4bd1cf8c7f848bfb9d5773588031d9efcb32ac
shabbarcendas <139884183+shabbarcendas@users.noreply.github.com>
2025-05-21 23:50:48 +0500

    wip: test scores

--- includes/class-controller.php
+++ includes/class-controller.php
@@ -23,6 +23,9 @@ class Controller
 
         add_action('wp_ajax_attd_get_study_hours', array($this, 'ajax_get_study_hours'));
         add_action('wp_ajax_attd_set_study_hours', array($this, 'set_study_hours'));
+
+
+        add_action('wp_ajax_attd_get_test_scores', array($this, 'get_test_scores'));
     }
 
     public function ajax_get_study_hours()
@@ -32,19 +35,54 @@ class Controller
         ajax_return(true, 'Study Hours', ['manual' => $manual_hours]);
     }
 
-    public function set_study_hours()
+    public function get_test_scores()
+    {
+//        $user_id = get_current_user_id();
+//        $test_score = 0;
+//        $course_ids = learndash_user_get_enrolled_courses($user_id);
+//        $attemps = learndash_get_user_quiz_attempt($user_id);
+//        foreach ($course_ids as $course_id) {
+//            $course = get_post($course_id);
+//            // Get course-level quizzes
+//            $course_quizzes = learndash_get_course_quiz_list($course, $user_id);
+//            foreach ($course_quizzes as $quiz) {
+//                if ($quiz['status'] == "completed") {
+//                    $course_data['course_quizzes'][] = [
+//                        'id' => $quiz['id'],
+//                        'title' => get_the_title($quiz['id']),
+//                    ];
+//                }
+//            }
+//
+//            // Get all lessons in the course
+//            $lessons = learndash_get_lesson_list($course_id);
+//            foreach ($lessons as $lesson) {
+//                $lesson_quizzes = learndash_get_lesson_quiz_list($lesson->ID, $user_id);
+//                foreach ($lesson_quizzes as $quiz){
+//                    $a = $quiz;
+//                }
+//            }
+//        }
+
+        ajax_return(true, 'Test Scores', 11);
+    }
+
+
+    function set_study_hours()
     {
         update_user_meta(get_current_user_id(), $this->pre . 'study_hours', $_POST['hours']);
         ajax_return(true, 'Study Hours', $_POST['hours']);
     }
 
-    public function ajax_set_exam_date()
+    public
+    function ajax_set_exam_date()
     {
         update_user_meta(get_current_user_id(), $this->pre . 'exam_date', $_POST['date']);
         ajax_return(true, 'Exam Date', $_POST['date']);
     }
 
-    public function ajax_get_exam_date()
+    public
+    function ajax_get_exam_date()
     {
         ajax_return(true, 'Exam Date', get_user_meta(get_current_user_id(), $this->pre . 'exam_date', true));
     }
--- src/components/TestScores.vue
+++ src/components/TestScores.vue
@@ -1,44 +1,18 @@
 <script lang="ts" setup>
 
 //@ts-ignore
-import Swal from 'sweetalert2';
 import {onMounted, ref} from "vue";
 import axios from "axios";
-import moment from "moment-timezone";
 
-const examDate = ref<number>(0);
+const testScores = ref<number>(0);
 
 onMounted(async () => {
   //@ts-ignore
   const response = await axios.postForm(acethetest_dashboard_script.ajax_url, {
-    action: 'attd_get_exam_date'
+    action: 'attd_get_test_scores'
   });
-  examDate.value = response.data.data ?? false;
+  testScores.value = response.data.data ?? false;
 })
-
-async function handleExamDateEdit() {
-  const {value: date} = await Swal.fire({
-    title: "Select Exam Date",
-    input: "date",
-    showCloseButton: true,
-    showCancelButton: true,
-    didOpen: () => {
-      const today = (new Date()).toISOString();
-      Swal.getInput().min = today.split("T")[0];
-    }
-  });
-
-  if (date) {
-    const timestamp = moment(date, 'YYYY-MM-DD').unix();
-    examDate.value = timestamp;
-    //@ts-ignore
-    await axios.postForm(acethetest_dashboard_script.ajax_url, {
-      action: 'attd_set_exam_date',
-      date: timestamp
-    });
-
-  }
-}
 
 </script>
 
@@ -49,9 +23,7 @@ async function handleExamDateEdit() {
       <span class="dashicons dashicons-analytics"></span>
     </div>
     <div class="flex gap-2">
-      <div class="font-bold text-xl">0</div>
-      <span class="dashicons dashicons-edit text-white bg-blue-400 rounded-full cursor-pointer"
-            @click="handleExamDateEdit"></span>
+      <div class="font-bold text-xl">{{ testScores }}</div>
     </div>
   </div>
 </template>
