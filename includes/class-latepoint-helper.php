ee71862aa9e09ec0df6ad9badb5d6a2ff0f93506
Syed Jawad <contact.syedjawad@gmail.com>
2025-05-28 19:39:10 +0500

    Update class-latepoint-helper.php

--- includes/class-latepoint-helper.php
+++ includes/class-latepoint-helper.php
@@ -5,15 +5,13 @@ class Latepoint_Helper
 {
     public static function get_study_hours_for_user($user_id)
     {
-
         $customer = new \OsCustomerModel();
-        $customer->load_by_id($user_id);
-
-        if (!$customer->id) return null;
+        $customer = $customer->where( [ 'wordpress_user_id' => $user_id ] )->set_limit( 1 )->get_results_as_models();
+        if (!$customer->id) return [];
 
         // Get bookings for this customer
         $bookings = new \OsBookingModel();
-        return $bookings->should_not_be_cancelled()->where(['customer_id' => $customer->id])->get_results_as_models();
+        return $bookings->should_not_be_cancelled()->where(['customer_id' => $customer->id ,'status' => LATEPOINT_BOOKING_STATUS_COMPLETED])->get_results_as_models();
     }
 
 }
