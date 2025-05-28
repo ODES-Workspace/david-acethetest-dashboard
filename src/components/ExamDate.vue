1098ab2efb629fe47be2b9e24b96efa1813d3c6a
shabbarcendas <139884183+shabbarcendas@users.noreply.github.com>
2025-05-28 17:01:30 +0500

    exam ciritical color done.

--- src/components/ExamDate.vue
+++ src/components/ExamDate.vue
@@ -2,7 +2,7 @@
 
 //@ts-ignore
 import Swal from 'sweetalert2';
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import axios from "axios";
 import moment from "moment-timezone";
 
@@ -16,10 +16,25 @@ onMounted(async () => {
   examDate.value = response.data.data ?? false;
 })
 
+const isExamCritical = computed(() => {
+  if (!examDate.value) return false;
+
+  const now = moment();
+  const exam = moment.unix(examDate.value);
+
+  const diffHours = exam.diff(now, 'hours');
+
+  // If exam date is in past 24 hours or next 24 hours, mark critical
+  return diffHours >= -24 && diffHours <= 24;
+});
 async function handleExamDateEdit() {
+  const currentDate = examDate.value
+      ? moment.unix(examDate.value).format('YYYY-MM-DD')
+      : '';
   const {value: date} = await Swal.fire({
     title: "Select Exam Date",
     input: "date",
+    inputValue: currentDate,
     showCloseButton: true,
     showCancelButton: true,
     didOpen: () => {
@@ -49,8 +64,10 @@ async function handleExamDateEdit() {
       <span class="dashicons dashicons-calendar"></span>
     </div>
     <div class="flex gap-2">
-      <div v-if="examDate" class="font-bold text-xl">{{ moment.unix(examDate).format("MM/DD/YYYY") }}</div>
-      <div class="text-red-400" v-else>Not set</div>
+      <div v-if="examDate" :class="['font-bold text-xl', isExamCritical ? 'text-red-500' : '']"
+      >{{ moment.unix(examDate).format("MM/DD/YYYY") }}
+      </div>
+      <div v-else class="text-red-400">Not set</div>
       <span class="dashicons dashicons-edit text-white bg-blue-400 rounded-full cursor-pointer"
             @click="handleExamDateEdit"></span>
     </div>
